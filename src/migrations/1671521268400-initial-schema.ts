f5ac1ff1107d69721cf2b1119f0aa7923f98a585
tirefeet <mehmetemin7@outlook.com>
2022-12-20 15:20:52 +0300

    feat: attempting migration

--- src/migrations/1671521268400-initial-schema.ts
+++ src/migrations/1671521268400-initial-schema.ts
@@ -1,7 +1,142 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import { query } from 'express';
+import { identity } from 'rxjs';
+import {
+  createQueryBuilder,
+  getRepository,
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableColumn,
+  TableForeignKey,
+} from 'typeorm';
 
 export class initialSchema1671521268400 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<void> {}
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'increment',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            isNullable: false,
+          },
+          {
+            name: 'username',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'gender',
+            type: 'varchar',
+          },
+          {
+            name: 'photo',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
 
-  public async down(queryRunner: QueryRunner): Promise<void> {}
+    await queryRunner.createTable(
+      new Table({
+        name: 'blogs',
+        columns: [
+          {
+            name: 'id',
+            type: 'increment',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            isNullable: false,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'content',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+
+    await queryRunner.createTable(
+      new Table({
+        name: 'profiles',
+        columns: [
+          {
+            name: 'id',
+            type: 'increment',
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: 'increment',
+            isNullable: false,
+          },
+          {
+            name: 'gender',
+            type: 'varchar',
+          },
+          {
+            name: 'photo',
+            type: 'varchar',
+          },
+        ],
+      }),
+    );
+
+    // one to many: user may have multiple blogs
+    await queryRunner.addColumn(
+      'blogs',
+      new TableColumn({
+        name: 'userId',
+        type: 'increment',
+      }),
+    );
+
+    await queryRunner.createForeignKey(
+      'blogs',
+      new TableForeignKey({
+        columnNames: ['userId'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'users',
+        onDelete: 'CASCADE',
+      }),
+    );
+
+    // one to one: user has a profile
+    await queryRunner.query(`SELECT id
+    FROM profiles
+    LEFT JOIN users
+    ON profiles.id = users.id`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    const table = await queryRunner.getTable('blogs');
+    const foreignKey = table.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf('userId') !== -1,
+    );
+
+    await queryRunner.dropForeignKey('blogs', foreignKey);
+    await queryRunner.dropColumn('blogs', 'userId');
+    await queryRunner.dropTable('blogs');
+    await queryRunner.dropTable('users');
+    await queryRunner.dropTable('profiles');
+  }
 }
--- src/user/models/user.entity.ts
+++ src/user/models/user.entity.ts
@@ -1,33 +1,38 @@
-import { Blog } from "src/blog/models/blog.entity";
-import { Profile } from "src/profile/models/profile.entity";
-import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn,OneToMany } from "typeorm";
+import { Blog } from 'src/blog/models/blog.entity';
+import { Profile } from 'src/profile/models/profile.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+  OneToMany,
+} from 'typeorm';
 
+@Entity({ name: 'users' })
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
 
-@Entity({name:'users'})
-export class User{
-    @PrimaryGeneratedColumn()
-    id:number
+  @Column({ unique: true, nullable: false })
+  username: string;
 
-    @Column({unique:true,nullable:false})
-    username: string
+  @Column({ nullable: false })
+  password: string;
 
-    @Column({nullable:false})
-    password:string
+  @Column({ unique: true, nullable: false })
+  email: string;
 
-    @Column({unique:true,nullable:false})
-    email:string
+  @Column()
+  gender: string;
 
-    @Column()
-    gender:string
+  @Column()
+  photo: string;
 
-    @Column()
-    photo:string
+  @OneToOne(() => Profile)
+  @JoinColumn()
+  profile: Profile;
 
-    @OneToOne(() => Profile)
-    @JoinColumn()
-    profile: Profile
-
-    @OneToMany(() => Blog, (b) => b.user)
-    blogs: Blog[]
-
+  @OneToMany(() => Blog, (b) => b.user, { cascade: true, eager: true })
+  blogs: Blog[];
 }
