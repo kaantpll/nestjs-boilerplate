87860bdedf9c552dc1d733b95220653293e9ece1
tirefeet <mehmetemin7@outlook.com>
2022-12-20 10:16:17 +0300

    chore: publish branch

--- src/main.ts
+++ src/main.ts
@@ -14,17 +14,17 @@ async function bootstrap() {
     .setVersion('1.0')
     .addTag('Users')
     .build();
-    
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.useGlobalInterceptors(new LoggingInterceptor());
-  
-  const  httpAdapter  = app.get(HttpAdapterHost);
+
+  const httpAdapter = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
-  
-  await app.listen(3000);
+
+  await app.listen(3002);
 }
 bootstrap();
