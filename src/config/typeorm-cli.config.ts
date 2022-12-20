880e392c0dd7b7bd56fea2b7d6fd6e0c344d7cdc
tirefeet <mehmetemin7@outlook.com>
2022-12-20 10:53:00 +0300

    fix: adjusted environment variable names

--- src/config/typeorm-cli.config.ts
+++ src/config/typeorm-cli.config.ts
@@ -9,12 +9,12 @@ const configService = new ConfigService();
 
 export default new DataSource({
   type: 'postgres',
-  host: configService.get<string>('DB_HOST'),
-  port: configService.get<number>('DB_PORT'),
-  username: configService.get<string>('DB_USERNAME'),
-  password: configService.get<string>('DB_PASSWORD'),
-  database: configService.get<string>('DB_NAME'),
-  logging: configService.get<boolean>('DB_LOGGING'),
+  host: configService.get<string>('DATABASE_HOST'),
+  port: configService.get<number>('DATABASE_PORT'),
+  username: configService.get<string>('DATABASE_USERNAME'),
+  password: configService.get<string>('DATABASE_PASSWORD'),
+  database: configService.get<string>('DATABASE_NAME'),
+  logging: configService.get<boolean>('DATABASE_LOGGING'),
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   migrations: [initialSchema1671521268400],
 });
